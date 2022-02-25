9b24830e2bfc9f18e53fb479cf4d018462b08da6
sans-connaissance <davemalicke@gmail.com>
2022-02-24 19:44:27 -0500

    CONNECT calendar app to ical

---
+++ CalendarTutorial/CalendarViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  CalendarTutorial
+//
+//  Created by David Malicke on 2/24/22.
+//
+
+import UIKit
+import CalendarKit
+import EventKit
+
+class CalendarViewController: DayViewController {
+    
+    private let eventStore = EKEventStore()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Calendar"
+        requestAccessToCalendar()
+        
+    }
+    
+    func requestAccessToCalendar() {
+        eventStore.requestAccess(to: .event) { success, error in
+            
+        }
+    }
+    
+    override func eventsForDate(_ date: Date) -> [EventDescriptor] {
+        let startDate = date
+        var oneDayComponents = DateComponents()
+        oneDayComponents.day = 1
+        
+        let endDate = calendar.date(byAdding: oneDayComponents, to: startDate)!
+        
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        let eventKitEvents = eventStore.events(matching: predicate)
+        let calendarKitEvents = eventKitEvents.map { ekEvent -> Event in
+            let ckEvent = Event()
+            ckEvent.dateInterval.start = ekEvent.startDate
+            ckEvent.dateInterval.end = ekEvent.endDate
+            ckEvent.isAllDay = ekEvent.isAllDay
+            ckEvent.text = ekEvent.title
+            if let eventColor = ekEvent.calendar.cgColor {
+                ckEvent.color = UIColor(cgColor: eventColor)
+            }
+            return ckEvent
+        }
+        return calendarKitEvents
+    }
+    
+}
+
